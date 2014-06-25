5d8827fbddd80155e6e31a148575af001b8d438f
tfitts <tfitts@gmail.com>
2014-06-25 13:41:29 -0600

    Change to ::Rail.env

--- lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
+++ lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
@@ -2,7 +2,7 @@ require 'paper_trail/version_concern'
 
 module PaperTrail
   class Version < ::ActiveRecord::Base
-    establish_connection "#{Rails.env}_post"
+    establish_connection "#{::Rails.env}_post"
     include PaperTrail::VersionConcern
   end
 end
